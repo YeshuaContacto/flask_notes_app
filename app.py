6fd5945791fc2c847995869b2908d82b58c4999d
Yeshua Sanchez <yeshuacontacto@gmail.com>
2025-09-01 08:51:36 -0600

    Se agrega ruta /nota

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+@app.route("/")
 def hello():
-    return "Hello world"
+    return "Hola mundooo"
+
+
+@app.route("/nota")
+def nota():
+    return "La aplicación es para que el usuario crea sus notas y luego se puedan cambiar o eliminar."
