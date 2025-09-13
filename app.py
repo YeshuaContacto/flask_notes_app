90e83f75606b6f4c6d5cadab2c3b07d73030177a
YeshuaContacto <104876539+YeshuaContacto@users.noreply.github.com>
2025-09-12 22:30:40 -0600

    Organización del proyecto

--- app.py
+++ app.py
@@ -1,29 +1,24 @@
-from flask import Flask, redirect, request, jsonify, render_template, url_for
+from flask import Flask, redirect, request, render_template, url_for
+
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from models import Note, db
 
 app = Flask(__name__)
-
-import os
-from flask_sqlalchemy import SQLAlchemy
-
-DB_FILE_PATH = os.path.join(
-    os.path.dirname(__file__),
-    "notes.sqlite"
-)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_FILE_PATH}"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db = SQLAlchemy(app)
+app.config.from_object(Config)
+db.init_app(app)
 
 
+@app.route("/acerca-de")
+def about():
+    return "Esto es una app de notas."
 
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.String(200), nullable=False)
 
-    def __repr__(self):
-        return f"<Note {self.id}: {self.title}>"
+@app.route("/contacto", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        return "formulario enviado correctamente", 201
+    return "Pagina de contacto"
 
 
 @app.route("/")
@@ -32,41 +27,13 @@ def home():
     return render_template("home.html", notes=notes)
 
 
-
-@app.route("/acerca-de")
-def about():
-    return "Esto es una app de notas."
-
-
-@app.route("/contacto", methods=['GET', 'POST'])
-def contact():
-    if request.method == "POST":
-        return "formulario enviado correctamente", 201
-    return "Pagina de contacto"
-
-
-@app.route("/api/info")
-def api_info():
-    data = {
-        "nombre": "Notes app",
-        "version": "1.1.1"
-    }
-    return jsonify(data), 200
-
-
-@app.route("/confirmation")
-def confirmation():
-    return "Prueba"
-
 @app.route("/crear-nota", methods=["GET", "POST"])
 def create_note():
     if request.method == "POST":
         title = request.form.get("title", "")
         content = request.form.get("content", "")
 
-        note_db = Note(
-            title=title, content=content
-        )
+        note_db = Note(title=title, content=content)
 
         db.session.add(note_db)
         db.session.commit()
@@ -75,7 +42,7 @@ def create_note():
     return render_template("note_form.html")
 
 
-@app.route('/editar-nota/<int:id>', methods=["GET", "POST"])
+@app.route("/editar-nota/<int:id>", methods=["GET", "POST"])
 def edit_note(id):
     note = Note.query.get_or_404(id)
     if request.method == "POST":
@@ -89,7 +56,7 @@ def edit_note(id):
     return render_template("edit_note.html", note=note)
 
 
-@app.route('/eliminar-nota/<int:id>', methods=["POST"])
+@app.route("/eliminar-nota/<int:id>", methods=["POST"])
 def delete_note(id):
     note = Note.query.get_or_404(id)
     db.session.delete(note)
