da71795e97fc0746c3709399f59097825c14bf78
YeshuaContacto <104876539+YeshuaContacto@users.noreply.github.com>
2025-09-12 22:52:47 -0600

    Blueprint

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, redirect, request, render_template, url_for
+from flask import Flask, request, url_for
 
-from flask_sqlalchemy import SQLAlchemy
 from config import Config
-from models import Note, db
+from models import db
+from notes.routers import notes_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
-
+app.register_blueprint(notes_bp)
 
 @app.route("/acerca-de")
 def about():
@@ -21,44 +21,3 @@ def contact():
     return "Pagina de contacto"
 
 
-@app.route("/")
-def home():
-    notes = Note.query.all()
-    return render_template("home.html", notes=notes)
-
-
-@app.route("/crear-nota", methods=["GET", "POST"])
-def create_note():
-    if request.method == "POST":
-        title = request.form.get("title", "")
-        content = request.form.get("content", "")
-
-        note_db = Note(title=title, content=content)
-
-        db.session.add(note_db)
-        db.session.commit()
-
-        return redirect(url_for("home"))
-    return render_template("note_form.html")
-
-
-@app.route("/editar-nota/<int:id>", methods=["GET", "POST"])
-def edit_note(id):
-    note = Note.query.get_or_404(id)
-    if request.method == "POST":
-        title = request.form.get("title", "")
-        content = request.form.get("content", "")
-        note.title = title
-        note.content = content
-        db.session.commit()
-        return redirect(url_for("home"))
-
-    return render_template("edit_note.html", note=note)
-
-
-@app.route("/eliminar-nota/<int:id>", methods=["POST"])
-def delete_note(id):
-    note = Note.query.get_or_404(id)
-    db.session.delete(note)
-    db.session.commit()
-    return redirect(url_for("home"))
