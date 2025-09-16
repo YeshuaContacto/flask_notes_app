38155d2f7f402d1cddd568db487783712eb5edd8
YeshuaContacto <104876539+YeshuaContacto@users.noreply.github.com>
2025-09-15 21:55:07 -0600

    Autenticación basica

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import Flask, request, url_for
 from config import Config
 from models import db
 from notes.routers import notes_bp
+from auth.routes import auth_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 app.register_blueprint(notes_bp)
+app.register_blueprint(auth_bp)
 
 with app.app_context():
     db.create_all()
---
+++ auth/routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, request, render_template, redirect, url_for, flash, session
+
+auth_bp = Blueprint("auth", __name__)
+
+@auth_bp.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        if username == "admin":
+            session["user"] = username
+            return redirect(url_for("notes.home"))
+        else:
+            flash("Usuario no permitido", "error")
+    return render_template("login.html")
