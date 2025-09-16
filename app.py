0733ddfe0bd52273c69fc502a44c506660140aac
YeshuaContacto <104876539+YeshuaContacto@users.noreply.github.com>
2025-09-15 23:05:13 -0600

    Creación y configuración de prueba unitaria

--- app.py
+++ app.py
@@ -1,28 +1,29 @@
-from flask import Flask, request, url_for
+from flask import Flask, request
 
 from config import Config
 from models import db
 from notes.routers import notes_bp
 from auth.routes import auth_bp
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db.init_app(app)
-app.register_blueprint(notes_bp)
-app.register_blueprint(auth_bp)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    db.init_app(app)
+    app.register_blueprint(notes_bp)
+    app.register_blueprint(auth_bp)
 
-with app.app_context():
-    db.create_all()
+    with app.app_context():
+        db.create_all()
 
-@app.route("/acerca-de")
-def about():
-    return "Esto es una app de notas."
-
-
-@app.route("/contacto", methods=["GET", "POST"])
-def contact():
-    if request.method == "POST":
-        return "formulario enviado correctamente", 201
-    return "Pagina de contacto"
+    @app.route("/acerca-de")
+    def about():
+        return "Esto es una app de notas."
 
 
+    @app.route("/contacto", methods=["GET", "POST"])
+    def contact():
+        if request.method == "POST":
+            return "formulario enviado correctamente", 201
+        return "Pagina de contacto"
+
+    return app
